ee15dde77177e640c159bea2f1b10b460c198d65
Pankaj Raj <75722183+Ipankaj07@users.noreply.github.com>
2021-12-18 11:17:03 +0530

    nav homePage return

--- main/src/index.js
+++ main/src/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static('public'));
 app.set('view engine','ejs');
 
-app.use('/',homeController);
+app.use('/home',homeController);
 
 app.use('/products', productController);
 app.use("/cart", cartController);
