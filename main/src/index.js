dd6b24395bc9f2effa4f24457efd60b20d8ca513
Pankaj Raj <75722183+Ipankaj07@users.noreply.github.com>
2021-12-18 11:01:03 +0530

    Update index.js

--- main/src/index.js
+++ main/src/index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const connect = require('./configis/db');
+
+const homeController = require('./controller/home.controller');
 const productController = require('./controller/product.controller');
+const cartController = require('./controller/cart.controller');
 
 app.use(express.json());
 app.use(express.static('public'));
 app.set('view engine','ejs');
+
+app.use('/',homeController);
 
 app.use('/products', productController);
 app.use("/cart", cartController);
