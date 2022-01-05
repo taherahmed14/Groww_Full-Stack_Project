a96d26812bda5472d91e21d6a7729f6be07be39c
taherahmed14 <taherahmed.sj.95@gmail.com>
2022-01-05 18:52:43 +0530

    added nodemon

--- main/src/index.js
+++ main/src/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static('public'));
 app.set('view engine','ejs');
 
-const port = process.env.port || 7500;
+const port = process.env.PORT || 7500;
 
 app.use('/home',homeController);
 app.use('/products', productController);
